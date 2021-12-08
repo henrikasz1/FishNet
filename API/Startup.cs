3f9a450d0bb9fdbdfea21bc0726608f3053bc6fd
pawleq <p.mykolaitis243@gmail.com>
2021-12-08 10:58:03 +0200

    New branch

--- API/Startup.cs
+++ API/Startup.cs
@@ -94,7 +94,7 @@ namespace API
             app.UseAuthentication();
 
             app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
